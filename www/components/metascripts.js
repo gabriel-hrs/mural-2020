505270bb743b04892ddc9bce1e322bcb77ce2d7d
Kelvin Iaguchi <kel.kazuo@outlook.com>
2020-02-20 21:47:00 -0300

    index.css - inutilizando o SASS

--- www/components/metascripts.js
+++ www/components/metascripts.js
@@ -9,7 +9,7 @@ function makeHead(title, exceptionsCSS, exceptionsJS){
     <title>${title}</title>
 
     <!-- ============== STYLES ================= -->
-    <link rel="stylesheet" type="text/css" href="../assets/css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     `
     if(exceptionsCSS !== undefined){
