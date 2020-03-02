a7024932ffef36ad031dd7ab992431538124162c
Gabriel Henrique <gabriel.henrique7087@gmail.com>
2020-03-02 01:45:25 -0300

    Envio de tema para o firebase

--- www/js/config-tema.js
+++ www/js/config-tema.js
@@ -6,52 +6,44 @@ $(document).ready(function() {
     firebase.auth().onAuthStateChanged( function( user ) {
         if ( user ) {
             var uid = user.uid;
+            var nome = user.nome;
 
             /* Função de cadastro de tema */
-            var imagem = $( "#imagem" );
-            var nome = $( "#nome-tema" );
-            var descricao = $( "#descricao-tema" );
-            var referencia = $( ".referencia:checked" );
-            var formato = $( "#toggle-check" );
-
-            imagem.change(function(){
+            $( "#imagem" ).change(function(){
                 $( ".perfil" ).removeClass("default-photo");
             });
 
-            imagem.on('change',function(e){
-
+            imagem = "";
+            nomeImagem = "";
+            $( "#imagem" ).on('change',function(e){
                 let file = e.target.files[0];
             
                 let reader = new FileReader();
                 reader.onload = function (element) {
                   $('.btn-circle').css({'background-image':`url('${element.target.result}')`});
                 };
-                
                 reader.readAsDataURL(file);
             
-                image = file;
-                imageName = file.name;
-              });
-            
-            // $( ".page-title" ).text( `Cadastro ${localStorage.getItem( "userType" )}` ); // Exibir do LocalStorage o tipo de usuário no título da página
+                imagem = file;
+                nomeImagem = file.name;
+            });
 
             $( ".btn-confirm" ).on( "click", function( novoTema ) {
                 novoTema.preventDefault();
 
-                createTema(
-                    firebase.auth().currentUser.uid,
-                    imagem.val(),
-                    nome.val(),
-                    descricao.val(),
-                    referencia.val(),
-                    formato.val()
-                );
+                let data_tema = {
+                    uid: firebase.auth().currentUser.uid,
+                    // nome_professor: nome,
+                    imagem: $( "#imagem" ).val(),
+                    nome: $( "#nome-tema" ).val(),
+                    descricao: $( "#descricao-tema" ).val()
+                };
+    
+                let data_imagem = {
+                    imagem: $( "#imagem" ).val()
+                };
 
-                createImagesTema(
-                    imagem.val()
-                );
-        
-                let setStorage = firebase.storage().ref( `${firebase.auth().currentUser.uid}/Temas/${imageName}` ).put( image ); // Guardar no LocalStorage a foto do usuário
+                let setStorage = firebase.storage().ref( `${firebase.auth().currentUser.uid}/Temas/${nomeImagem}` ).put( imagem ); // Guardar no LocalStorage a foto do usuário
         
                 setStorage.on( "state_changed",
                 function ProgressEvent( snapshot ) {
@@ -64,38 +56,18 @@ $(document).ready(function() {
                 function complete() { 
                     alert( "Upload realizado com sucesso!" );
                     window.location.assign( "temas.html" );
-                });
+                });                    
 
-                function createTema( uid, imagem, nome, descricao, referencia, formato ) {
-                    let data = {
-                        uid: uid,
-                        imagem: imagem,
-                        nome: nome,
-                        descricao: descricao,
-                        referencia: referencia,
-                        formato: formato
-                    };
-                    return firebase
-                    .database()
-                    .ref()
-                    .child( "Temas" + nome )
-                    .set( data );
+                function sendData() {
+                    firebase.database().ref().child( "Temas/" + data_tema.nome ).set( data_tema );
+                    firebase.database().ref().child( "Usuarios/" + firebase.auth().currentUser.uid + "/Imagens/Temas" ).set( data_imagem );
                 }
 
-                function createImagesTema( imagem ){
-                    let data = {
-                      imagem: imagem
-                    };
-                    return firebase
-                    .database()
-                    .ref()
-                    .child( "Usuarios/" + firebase.auth().currentUser.uid + "/Imagens/Temas" )
-                    .set( data );
-                  }
+                return sendData();
+                // Para enviar os dados preciso retornar a função sendData, mas assim não redireciona a página
+                // Caso tente redirecionar antes, a função não é executada pois já está em outra página
+                window.location.assign( "temas.html" );
             });
         }
     });   
 });
-
-/* Exibir dados do usuário nessa página */
-// import "dados-usuario.js";
