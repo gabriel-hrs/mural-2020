bb78ce23f74a4bd3d7f836d4df587029d5b42de0
Gabriel Henrique <gabriel.henrique7087@gmail.com>
2020-03-02 21:38:48 -0300

    Turma e tema - exibição das turmas e cadastro do tema com a url da imagem, exibição de temas com a própria imagem

--- www/js/turmas.js
+++ www/js/turmas.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
             var storageRef = storage.ref();
             var db = firebase.database();
 
+            /* Foto de perfil */
             db.ref( `Usuarios/${uid}/Imagens/Perfil` ).on( "value", function( snapshot ) {
                 snapshot.forEach( function( item ) {
                     if( item.val() != "" ) {
@@ -19,12 +20,48 @@ $(document).ready(function() {
                     $( ".top-bar .icon" ).attr( "src", downloadUrl );
                 });
             });
-        }
-    });
 
-    /* Botão Ver/Editar/Apagar Turma do tipo Toggle */
-    $( ".config-turma" ).on( "click" , function() {
-        $( ".turma-links" ).toggleClass( "toggle-left" );
-    });
+            /* Loop de turmas */
+            firebase.database().ref("Turmas").on("value", function(snapshot) {
+                $(".turmas-page .wrapper").html("");
+
+                var turmas = "<ul class='list-group turmas-list'>";
+               
+                snapshot.forEach(function( turma ) {
+                    turmas += "<li class='list-group-item'>";
+                    turmas += "<ul class='turma-links'>";
+                    turmas += "<li class='action-link'>";
+                    turmas += "<img class='icon' src='img/icon-info.svg' alt='Ícone para ver informações da turma'>";
+                    turmas += "</li>";
+                    turmas += "<li class='action-link'>";
+                    turmas += "<img class='icon' src='img/icon-edit-post.svg' alt='Ícone para editar detalhes da turma'>";
+                    turmas += "<a href='config-turma.html'>Editar</a>";
+                    turmas += "</li>";
+                    turmas += "<li class='action-link'>";
+                    turmas += "<img class='icon' src='img/icon-delete.svg' alt='Ícone para excluir turma'>";
+                    turmas += "<a href='#'>Apagar</a>";
+                    turmas += "</li>";
+                    turmas += "</ul>";
+                    turmas += "<a href='#' class='config-turma'>";
+                    turmas += "<img class='icon' src='img/icon-edit.svg' alt='Ícone de configuração da turma'>";
+                    turmas += "</a>";
+                    turmas += `<span class='turma-school'>${turma.val().escola}</span>`;
+                    turmas += `<h2 class='turma-name'>${turma.val().serie}</h2>`;
+                    turmas += `<span class="turma-local">${turma.val().cidade}</span>`;
+                    turmas += "<a class='star-icon'>";
+                    turmas += "<div class='icon' style='mask-image: url(img/icon-star.svg); -webkit-mask-image: url(img/icon-star.svg);' alt='Ícone para favoritar turma'></div>";
+                    turmas += "</a>";
+                });
+                
+                turmas += "</ul>";
+                $(".turmas-page .wrapper").html(turmas);
+            });    
+        }
+
+        /* Botão Ver/Editar/Apagar Turma do tipo Toggle */
+        $( ".config-turma" ).on( "click" , function() {
+            $( this ).siblings().toggleClass( "toggle-left" );
+        });
+    });    
 });
 
